24bab99999e07aa783b5ab48ce5efae4a9f7e46a
Tonyfu09 <39818274+Tonyfu09@users.noreply.github.com>
2025-02-11 19:49:24 -0800

    versioning enable

--- terraform-aws-bucketpolicy.tf
+++ terraform-aws-bucketpolicy.tf
@@ -8,12 +8,18 @@ provider "aws" {
 resource "aws_s3_bucket" "tony-web-app-a" {
   bucket = "tony-web-app-a"  # Bucket name
   acl    = "private"         # Only owner has access
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
 
 # S3 Bucket for Tony's Web App B
 resource "aws_s3_bucket" "tony-web-app-b" {
   bucket = "tony-web-app-b"  # Bucket name
   acl    = "private"         # Only owner has access
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
 
 # S3 Bucket Policy for Tony's Web App A
