4e993b278a627c20ee8485b99c0f32c3e1a6a5c6
Tonyfu09 <39818274+Tonyfu09@users.noreply.github.com>
2025-02-15 10:20:31 -0800

    update iam role

--- terraform-aws-bucketpolicy.tf
+++ terraform-aws-bucketpolicy.tf
@@ -34,6 +34,7 @@ resource "aws_s3_bucket" "tony-web-app-b-qa" {
 }
 
 # S3 Bucket Policy for Tony's Web App A (Dev Environment)
+# S3 Bucket Policy for Tony's Web App A - Dev
 resource "aws_s3_bucket_policy" "tony-web-app-a-dev-policy" {
   bucket = aws_s3_bucket.tony-web-app-a-dev.id
 
@@ -43,13 +44,13 @@ resource "aws_s3_bucket_policy" "tony-web-app-a-dev-policy" {
   "Id": "tony-web-app-a-dev-policy",
   "Statement": [
     {
-      "Sid": "AllowSpecificIP",
+      "Sid": "AllowIP",
       "Effect": "Allow",
       "Principal": "*",
       "Action": "s3:GetObject",
       "Resource": "arn:aws:s3:::tony-web-app-a-dev/*",
       "Condition": {
-        "IpAddress": { "aws:SourceIp": "24.85.136.163/32" }
+        "IpAddress": {"aws:SourceIp": "192.168.10.0/24"}
       }
     },
     {
@@ -59,13 +60,30 @@ resource "aws_s3_bucket_policy" "tony-web-app-a-dev-policy" {
       "Action": "s3:*",
       "Resource": "arn:aws:s3:::tony-web-app-a-dev/*",
       "Condition": {
-        "IpAddress": { "aws:SourceIp": "0.0.0.0/0" }
+        "IpAddress": {"aws:SourceIp": "0.0.0.0/0"}
       }
+    },
+    {
+      "Sid": "AllowDevGroupAccess",
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "arn:aws:iam::400000000003:group/Dev"
+      },
+      "Action": [
+        "s3:ListBucket",
+        "s3:GetObject",
+        "s3:PutObject"
+      ],
+      "Resource": [
+        "arn:aws:s3:::tony-web-app-a-dev",
+        "arn:aws:s3:::tony-web-app-a-dev/*"
+      ]
     }
   ]
 }
 POLICY
 }
+
 
 # S3 Bucket Policy for Tony's Web App B (QA/Testing Environment)
 resource "aws_s3_bucket_policy" "tony-web-app-b-qa-policy" {
@@ -83,7 +101,7 @@ resource "aws_s3_bucket_policy" "tony-web-app-b-qa-policy" {
       "Action": "s3:GetObject",
       "Resource": "arn:aws:s3:::tony-web-app-b-qa/*",
       "Condition": {
-        "IpAddress": { "aws:SourceIp": "24.85.136.163/32" }
+        "IpAddress": { "aws:SourceIp": "192.168.20.0/24" }
       }
     },
     {
@@ -96,6 +114,22 @@ resource "aws_s3_bucket_policy" "tony-web-app-b-qa-policy" {
         "IpAddress": { "aws:SourceIp": "0.0.0.0/0" }
       }
     }
+    {
+      "Sid": "AllowQAGroupAccess",
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "arn:aws:iam::400000000003:group/QA"
+      },
+      "Action": [
+        "s3:ListBucket",
+        "s3:GetObject",
+        "s3:PutObject"
+      ],
+      "Resource": [
+        "arn:aws:s3:::tony-web-app-b-qa",
+        "arn:aws:s3:::tony-web-app-b-qa/*"
+      ]
+    }	
   ]
 }
 POLICY
